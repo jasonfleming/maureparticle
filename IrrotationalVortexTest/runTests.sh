3a8b87c5fee153dfd270aa226a1b6ebc39c804f6
natedill <natedill@gmail.com>
2020-05-02 21:10:06 -0400

    added to irrotational vortex examples

--- IrrotationalVortexTest/runTests.sh
+++ IrrotationalVortexTest/runTests.sh
@@ -1,5 +1,6 @@
 
 ./maurpt_lose_wetdry.exe --maureparticleoutputfile Euler-Ts30-Steady.xyz  --maureparameterinputfile Euler-Ts30-Steady.inp
+./maurpt_lose_wetdry.exe --maureparticleoutputfile Euler-Ts30-DYN.xyz  --maureparameterinputfile Euler-Ts30-DYN.inp
 ./maurpt_lose_wetdry.exe      --maureparticleoutputfile Euler-Ts1-Steady.xyz   --maureparameterinputfile Euler-Ts1-Steady.inp
 ./maurpt_lose_wetdry.exe --maureparticleoutputfile RK-Ts30-Steady.xyz     --maureparameterinputfile RK2-Ts30-Steady.inp
 ./maurpt_lose_wetdry.exe --maureparticleoutputfile RK-Ts30-DYN.xyz        --maureparameterinputfile RK2-Ts30-DYN.inp
